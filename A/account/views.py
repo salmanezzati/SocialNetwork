00fde3bae012cdbd6090e967f17b4e7f1ac24856
salmanezzati <salmanezzati@gmail.com>
2023-02-11 19:28:26 +0330

    Add all files of project to GitHub 11_02_2023

---
+++ A/account/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render, redirect
+from django.views import View
+from .forms import UserRegistrationForm, UserLoginForm
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+
+
+class UserRegisterView(View):
+    form_class = UserRegistrationForm
+    template_name = 'account/register.html'
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, {'form':form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            User.objects.create_user(cd['username'], cd['email'], cd['password1'])
+            messages.success(request, 'You registered successfully', 'success')
+            return redirect('home:home')
+        return render(request, self.template_name, {'form':form})
+
+
+class UserLoginView(View):
+    form_class = UserLoginForm
+    temlate_name = 'account/login.html'
+
+    def get(self, request):
+        form = self.form_class
+        return render(request, self.temlate_name, {'form':form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, username=cd['username'], password=cd['password'])
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'You logged in successfully', 'success')
+                return redirect('home:home')
+            messages.error(request, 'Usernsme or password is wrong', 'warning')
+        return render(request, self.temlate_name, {'form':form})
+
+
+class UserLogoutView(View):
+    def get(self, request):
+        logout(request)
+        messages.success(request, 'You logged out successfully', 'success')
+        return redirect('home:home')
+
